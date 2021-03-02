1c59247662166a376206eaa81610be29cb6c2e87
robellm <72279738+robellm@users.noreply.github.com>
2021-03-02 22:01:16 +0100

    Create users.pp

---
+++ manifests/users.pp
@@ -0,0 +1,24 @@
+user { "bob":
+        ensure => present,
+        home => '/home/bob',
+        shell => '/bin/bash',
+        managehome => 'true',
+}
+user { "janet":
+        ensure => present,
+        home => '/home/present',
+        shell => '/bin/bash',
+        managehome => 'true',
+}
+user { "alice":
+        ensure => present,
+        home => '/home/alice',
+        shell => '/bin/bash',
+        managehome => 'true',
+}
+user { "tim":
+        ensure => present,
+        home => '/home/tim',
+        shell => '/bin/bash',
+        managehome => 'true',
+}
