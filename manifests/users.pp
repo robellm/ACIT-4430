f8a8c34465c92458d9efdb34e79a356a3b231285
robellm <72279738+robellm@users.noreply.github.com>
2021-03-03 20:11:20 +0100

    Update users.pp

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,29 +1,12 @@
-user { "bob":
+$users = ['bob', 'janet', 'alice', 'tim']
+user { $users:
         ensure => present,
-        home => '/home/bob',
-        shell => '/bin/bash',
-        managehome => 'true',
-}
-user { "janet":
-        ensure => present,
-        home => '/home/present',
-        shell => '/bin/bash',
-        managehome => 'true',
-}
-user { "alice":
-        ensure => present,
-        home => '/home/alice',
-        shell => '/bin/bash',
-        managehome => 'true',
-}
-user { "tim":
-        ensure => present,
-        home => '/home/tim',
         shell => '/bin/bash',
         managehome => 'true',
 }
 
-group {"developers":
+$developers = ['janet', 'tim']
+group { $developers:
         ensure => present,
-        members => 'janet', 'tim'
+        gid => 500,
 }
