d02a608dc36a8da63e534bf3014041996ff9b730
robellm <72279738+robellm@users.noreply.github.com>
2021-03-04 15:06:19 +0100

    Update developers.pp

--- manifests/developers.pp
+++ manifests/developers.pp
@@ -1,10 +1,3 @@
-/*$users = ['bob', 'janet', 'alice', 'time']
-user { $users:
-        ensure => present,
-        shell => '/bin/bash',
-        managehome => 'true',
-} */
-
 package { 'emacs':
         ensure => installed
 }
