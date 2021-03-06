bd4c4cc96401cb9166cb25bf8e940af190173db6
aunefyren <oystein.sverre@gmail.com>
2021-03-06 18:07:47 +0100

    Endret MLN gluster til storage, la til cron-job i storage.pp
    MLN prosjektet kalte fortsatt storage.pp fortsatt storage-puppet filen for 'GlusterFS.pp' istedenfor 'storage.pp'.
    
    La til cronjob for storage.pp, kommandoen er tatt fra MLN filen, den utføres hvert 5 min.

--- manifests/storage.pp
+++ manifests/storage.pp
@@ -1,3 +1,10 @@
 package {'glusterfs-server' :
         ensure => installed,
 }
+cron { 'apply':
+    command => 'sudo /opt/puppetlabs/puppet/bin/puppet apply storage.pp',
+    user => root,
+    hour => '*',
+    minute => '*/5',
+    ensure => present,
+}
