0b3cc20e08146247a55e0aeccbb1bcbd362d192f
robellm <72279738+robellm@users.noreply.github.com>
2021-03-19 00:56:46 +0100

    Update launch.sh

--- NEW/MLN.FILE/launch.sh
+++ NEW/MLN.FILE/launch.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-echo " "
-echo "Building MLN now ..."
-sleep 3
+echo -e "\n"
+echo " ... Building MLN now ... "
+sleep 5
 mln build -f ./project.mln
-echo " "
 
-echo " "
-echo "Starting MLN now ... "
+echo -e "\n"
+echo " ... Starting MLN now ... "
 sleep 2
 mln start -p project
-echo " "
-echo " PLEASE WAIT A FEW SEC'S FOR THE INSTANCES TO FULLY BE DEPOLOYED ... "
-sleep 40
-echo " THANKS FOR BEING PATIENT"
-sleep 5
-echo " "
 
-echo " "
-echo "The current status of MLN ... "
-echo " "
-sleep 2
+echo -e "\n"
+echo " ... PLEASE WAIT FOR A FEW SEC'S FOR THE INSTANCES TO FULLY BE DEPOLOYED ... "
+sleep 40
+echo " ... THANKS FOR BEING PATIENT ... "
+sleep 5
+
+echo -e "\n"
+echo " ... The current status of MLN ... "
+sleep 5
 mln status -p project
 
 VAR=true
@@ -27,52 +25,52 @@ VAR=true
 while $VAR; do
 if mln status -p project | grep -q "down"; then
     echo " "
-    echo "One or more instances are down ... "
-    echo "Rebuilding project and instances again..."
-    echo " "
-
+    echo " ... One or more instances are down ... "
+    sleep 3
+    echo " ... Rebuilding N Starting project and instances again ... "
     sleep 10
-    echo "PLEASE PRESS y then ENTER, WHEN REQUESTED!"
+    echo " ... PLEASE PRESS y then ENTER, WHEN REQUESTED! ...  "
+    echo " "
     mln remove -p project
     sleep 20
-    echo " "
 
+    echo -e "\n"
     mln build -f project.mln
     sleep 10
-    echo "PLEASE BE PATIENT"
-    echo "PLEASE PRESS y then ENTER, WHEN REQUESTED!"
-    sleep 3
-    mln start -p project
     echo " "
+    sleep 4
+    mln start -p project
     sleep 40
 
-    echo " "
-    echo "New status of the project"
-    echo " "
+    echo -e "\n"
+    echo " ... New status of the project ... "
+    sleep 3
     mln status -p project
+
 else
-    echo " "
-    echo "Finished"
-    echo "ALL instances are up & running correctly"
+    echo -e "\n"
+    echo " ... Finished ... "
+    sleep 3
+    echo " ... ALL instances are up & running correctly ... "
+    sleep 4
     VAR=false
 fi
 done
 
-echo " "
-echo "Signing CERTIFICATES FROM THE AGENTS.. PLEASE WAIT A FEW SEC'S"
-echo " "
-sleep 20
+echo -e "\n"
+echo " ... Signing CERTIFICATES FROM THE AGENTS .. PLEASE WAIT FOR A FEW SEC'S ... "
+sleep 65
+echo " ... THANKS FOR BEING PATIENT ... "
+sleep 15
 /opt/puppetlabs/puppet/bin/puppet cert sign --all
-echo " "
-echo " "
-sleep 5
-echo "Listing ALL the signed certificates"
-echo " "
-sleep 2
+
+echo -e "\n"
+echo " ... Listing ALL the signed certificates ... "
+sleep 4
 /opt/puppetlabs/puppet/bin/puppet cert list --all
-echo " "
+
+echo -e "\n"
+sleep 4
+echo " ... Listing all the IPs ... "
 sleep 3
-echo " "
-echo " Listing all the IPs "
-sleep 1
 mln status -p project
