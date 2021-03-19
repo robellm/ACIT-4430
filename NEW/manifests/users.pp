d8b8871502d67bd055f3892c5622ce7a2926711c
robellm <72279738+robellm@users.noreply.github.com>
2021-03-19 01:05:58 +0100

    Update users.pp
    in Ubuntu --> openssl passwd -1  
    
    # Enter your password here 
    Password: 
    Verifying - Password: 
     
    $1$HTumvYUGYUGwsxQxCp3F/nGc4DCYM

--- NEW/manifests/users.pp
+++ NEW/manifests/users.pp
@@ -1,46 +1,42 @@
 class users {
-        group { "developer":
+        group { 'developers':
                 ensure => present,
-                comment => "JANET & TIM GROUP",
-                shell => "/bin/bash",
-                home => "/home/developer",
-                managehome => "true",
         }
 
-        user { "bob":
+        user { 'bob':
                 ensure => present,
-                comment => "This is BOBBY's account",
-                shell => "/bin/bash",
-                home => "/home/bob",
-                groups => ["sudo"],
-                managehome => "true",
-                password => "bob",
-        }
-        user { "janet":
-                ensure => present,
-                comment => "JANET, FROM DEVELOPERS GROUP",
-                shell => "/bin/bash",
-                home => "/home/janet",
-                groups => ["sudo", "developer"],
-                managehome => "true",
-                password => "janet",
-        }
-        user { "alice":
-                ensure => present,
-                comment => "ALICE FROM WONDERLAND",
-                shell => "/bin/bash",
-                home => "/home/alice",
-                groups => ["sudo"],
-                managehome => "true",
-                password => "alice",
-        }
-        user { "tim":
-                ensure => present,
-                comment => "TIM, FROM DEVELOPERS GROUP",
-                shell => "/bin/bash",
-                home => "/home/tim",
-                groups => ["sudo", "developer"],
+                comment => 'This is BOBBYs account',
+                shell => '/bin/bash',
+                home => '/home/bob',
+                groups => ['sudo'],
                 managehome => 'true',
-                password => "tim",
+                password => '$1$u7o3O9lw$TU7rwH55qm6VfPCKaviuR.',
+        }
+        user { 'janet':
+                ensure => present,
+                comment => 'JANET, FROM DEVELOPERS GROUP',
+                shell => '/bin/bash',
+                home => '/home/janet',
+                groups => ['sudo', 'developers'],
+                managehome => 'true',
+                password => '$1$7RTvKFvl$E6B0/5SAmJJPGZVKMZCo71',
+        }
+        user { 'alice':
+                ensure => present,
+                comment => 'ALICE FROM WONDERLAND',
+                shell => '/bin/bash',
+                home => '/home/alice',
+                groups => ['sudo'],
+                managehome => 'true',
+                password => '$1$chU8epv4$gqEyE7wmRGOF85WO1Q06Y1',
+        }
+        user { 'tim':
+                ensure => present,
+                comment => 'TIM, FROM DEVELOPERS GROUP',
+                shell => '/bin/bash',
+                home => '/home/tim',
+                groups => ['sudo', 'developers'],
+                managehome => 'true',
+                password => '$1$xSXKSBo8$2ekmfp7mwsjcnmE.iCTHg/',
         }
 }
